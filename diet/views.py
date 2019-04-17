926e060ea33a0960d48b96cf61d540ddb6a1bac0
EspeIgira <Mapapuro2>
2019-04-17 10:50:52 +0200

    First page created

--- diet/views.py
+++ diet/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+
+def diet(request):
+    return render(request, 'index.html'))
+
+def health_of_day(request):
+    
+    return render(request, 'all-heath/today-health.html')
